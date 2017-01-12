67a675f962ccd58386febcdb782bddf61ea0c835
veLoc1ty47 <mcarstensen@protonmail.com>
2017-01-12 16:03:32 +0100

    Prøvet at rette så den rent faktisk regner rigtig ud, ikke lykkedes

--- test.fsx
+++ test.fsx
@@ -1,26 +1,29 @@
 type Planet () = class
-    let deltaT = 1.0
+    let deltaT = 0.1
     let mutable veloVector = (0.0, 0.0)
 
     // Beregner r
     let calcR (radius: float, long: float, lat: float) =
         let toRad n =
-            n/360.0*2.0*System.Math.PI
+            (n*System.Math.PI)/180.0
         
-        let p = ((radius) * sin(toRad (lat+90.0))*cos(toRad long), (radius) * sin(toRad (lat+90.0))*sin(toRad long))
-        p
+        ((radius) * sin(toRad (lat+90.0))*cos(toRad long), (radius) * sin(toRad (lat+90.0))*sin(toRad long))
 
     // Array der indeholder alle udregnte positioner for given planet
     let locationArray = Array.init 365 (fun x -> (calcR (0.98329, 100.6001, 0.0044)))
+
+    let GMSolen = 2.959122082322128*(10.0**(-4.0))
     
     // Beregner a_i
     member x.calcA r = 
-        (((2.959122082322128*10.0**(-4.0))/(((fst r)**2.0+(snd r)**2.0)**0.5)**3.0)*(fst r), ((2.959122082322128*10.0**(-4.0))/(((fst r)**2.0+(snd r)**2.0)**0.5)**3.0)*(snd r))
+        let lengthR = ((((fst r)**2.0)+((snd r)**2.0))**0.5)**3.0
+        (((GMSolen)/(lengthR))*(fst r), ((GMSolen)/(lengthR)*(snd r)))
 
     // Beregner V(t_{n+1})
     member x.calcV (velo : float * float) (accel : float * float) (deltaT : float) =
         match velo, accel with
-        | (a, b), (c, d) -> veloVector <- (a + c * deltaT, b + d * deltaT)
+        | (a, b), (c, d) -> 
+            veloVector <- (a + c * deltaT, b + d * deltaT)
 
     member x.pArray = locationArray
     
@@ -32,9 +35,10 @@ type Planet () = class
     // Beregner alle r 
     member x.position (n) = 
         for i = 1 to n do 
-            let s = (fst (locationArray.[i-1]) + fst (veloVector) * deltaT, snd (locationArray.[i-1]) + snd (veloVector) * deltaT)
-            locationArray.[i] <- s        
-            x.calcV veloVector (x.calcA s) deltaT 
+            locationArray.[i] <- (fst (locationArray.[i-1]) + fst (veloVector) * deltaT, snd (locationArray.[i-1]) + snd (veloVector) * deltaT)        
+            x.calcV veloVector (x.calcA (locationArray.[i-1])) deltaT 
+            printfn "A er: %A" (x.calcA (locationArray.[i-1]))
+            printfn "V ændret til: %A" veloVector
 end
 
 let bob = new Planet()
