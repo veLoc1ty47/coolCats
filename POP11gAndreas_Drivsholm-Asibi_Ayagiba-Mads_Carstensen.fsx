d5418079f6ba0c1ddb2379bcf8a5ee9c90c2b91f
veLoc1ty47 <mcarstensen@protonmail.com>
2017-01-12 14:35:48 +0100

    Lavet planet klasse

--- POP11gAndreas_Drivsholm-Asibi_Ayagiba-Mads_Carstensen.fsx
+++ POP11gAndreas_Drivsholm-Asibi_Ayagiba-Mads_Carstensen.fsx
@@ -5,48 +5,49 @@ type pen = Color * float
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let deltaT = 1.0
+type Planet () = class
+    let deltaT = 1.0
+    let mutable veloVector = (0.0, 0.0)
 
-// Beregner r_0
-let calcR (radius: float, long: float, lat: float) =
-    let toRad n =
-        n/360.0*2.0*System.Math.PI
+    // Beregner r
+    let calcR (radius: float, long: float, lat: float) =
+        let toRad n =
+            n/360.0*2.0*System.Math.PI
+        
+        let p = ((radius) * sin(toRad (lat+90.0))*cos(toRad long), (radius) * sin(toRad (lat+90.0))*sin(toRad long))
+        p
+
+    // Array der indeholder alle udregnte positioner for given planet
+    let locationArray = Array.init 365 (fun x -> (calcR (0.98329, 100.6001, 0.0044)))
     
-    let p = ((radius) * sin(toRad (lat+90.0))*cos(toRad long), (radius) * sin(toRad (lat+90.0))*sin(toRad long))
-    p
+    // Beregner a_i
+    member x.calcA r = 
+        (((2.959122082322128*10.0**(-4.0))/(((fst r)**2.0+(snd r)**2.0)**0.5)**3.0)*(fst r), ((2.959122082322128*10.0**(-4.0))/(((fst r)**2.0+(snd r)**2.0)**0.5)**3.0)*(snd r))
+
+    // Beregner V(t_{n+1})
+    member x.calcV (velo : float * float) (accel : float * float) (deltaT : float) =
+        match velo, accel with
+        | (a, b), (c, d) -> veloVector <- (a + c * deltaT, b + d * deltaT)
+
+    member x.pArray = locationArray
     
-////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Beregner r_0
+    member x.veloHelp (point1 : float * float * float) (point2 : float * float * float) =
+        match (calcR point1), (calcR point2) with
+        | (a, b), (c, d) -> veloVector <- (c-a, d-b) 
 
-// Beregner a_i
-let calcA r = 
-    (((2.959122082322128*10.0**(-4.0))/(((fst r)**2.0+(snd r)**2.0)**0.5)**3.0)*(fst r), ((2.959122082322128*10.0**(-4.0))/(((fst r)**2.0+(snd r)**2.0)**0.5)**3.0)*(snd r))
+    // Beregner alle r 
+    member x.position (n) = 
+        for i = 1 to n do 
+            let s = (fst (locationArray.[i-1]) + fst (veloVector) * deltaT, snd (locationArray.[i-1]) + snd (veloVector) * deltaT)
+            locationArray.[i] <- s        
+            x.calcV veloVector (x.calcA s) deltaT 
+end
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+let bob = new Planet()
 
-let mutable veloVector = (0.0, 0.0)
-
-let veloHelp (point1 : float * float * float) (point2 : float * float * float) =
-    match (calcR point1), (calcR point2) with
-    | (a, b), (c, d) -> veloVector <- (c-a, d-b) 
-
-// Beregner V(t_{n+1})
-let calcV (velo : float * float) (accel : float * float) (deltaT : float) =
-    match velo, accel with
-    | (a, b), (c, d) -> veloVector <- (a + c * deltaT, b + d * deltaT)
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-let p = Array.init 365 (fun x -> (calcR (0.98329, 100.6001, 0.0044)))
-
-veloHelp (0.98329, 100.6001, 0.0044) (0.98328, 101.6192, 0.0044)
-
-let position n = 
-    for i = 1 to n do 
-        let s = (fst (p.[i-1]) + fst (veloVector) * deltaT, snd (p.[i-1]) + snd (veloVector) * deltaT)
-        p.[i] <- s        
-        calcV veloVector (calcA s) deltaT 
-
-position 364
+bob.veloHelp (0.98329, 100.6001, 0.0044) (0.98328, 101.6192, 0.0044)
+bob.position 364
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -73,7 +74,7 @@ let mutable counter = 0
 
 let paintFunction() =
     win.Paint.Add (createEllipse Color.White point1 point2 point3 point4)
-    match p.[counter] with
+    match bob.pArray.[counter] with
     | (a,b) ->
         point1 <- int(round(a*100.0))-10+400
         if b > 0.0 then
--- test.fsx
+++ test.fsx
@@ -1,13 +1,17 @@
 type Planet () = class
     let deltaT = 1.0
     let mutable veloVector = (0.0, 0.0)
+
+    // Beregner r
     let calcR (radius: float, long: float, lat: float) =
         let toRad n =
             n/360.0*2.0*System.Math.PI
         
         let p = ((radius) * sin(toRad (lat+90.0))*cos(toRad long), (radius) * sin(toRad (lat+90.0))*sin(toRad long))
         p
-    let p = Array.init 700 (fun x -> (calcR (0.98329, 100.6001, 0.0044)))
+
+    // Array der indeholder alle udregnte positioner for given planet
+    let locationArray = Array.init 365 (fun x -> (calcR (0.98329, 100.6001, 0.0044)))
     
     // Beregner a_i
     member x.calcA r = 
@@ -18,23 +22,25 @@ type Planet () = class
         match velo, accel with
         | (a, b), (c, d) -> veloVector <- (a + c * deltaT, b + d * deltaT)
 
-    member x.pArray = p
+    member x.pArray = locationArray
     
+    // Beregner r_0
     member x.veloHelp (point1 : float * float * float) (point2 : float * float * float) =
         match (calcR point1), (calcR point2) with
         | (a, b), (c, d) -> veloVector <- (c-a, d-b) 
-    
+
+    // Beregner alle r 
     member x.position (n) = 
         for i = 1 to n do 
-            let s = (fst (p.[i-1]) + fst (veloVector) * deltaT, snd (p.[i-1]) + snd (veloVector) * deltaT)
-            p.[i] <- s        
+            let s = (fst (locationArray.[i-1]) + fst (veloVector) * deltaT, snd (locationArray.[i-1]) + snd (veloVector) * deltaT)
+            locationArray.[i] <- s        
             x.calcV veloVector (x.calcA s) deltaT 
 end
 
 let bob = new Planet()
+
 bob.veloHelp (0.98329, 100.6001, 0.0044) (0.98328, 101.6192, 0.0044)
-bob.position 699
+bob.position 364
 
 for elm in bob.pArray do
     printfn "%A" elm
-
